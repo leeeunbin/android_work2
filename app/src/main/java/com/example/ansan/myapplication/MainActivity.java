9b425e0e62ae400b1e0ed56ba12545091831d0cc
au <au2015@naver.com>
2016-11-02 11:04:28 +0900

    메인 액티비티 변경

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        //메인 액티비티 변경
     }
 }
